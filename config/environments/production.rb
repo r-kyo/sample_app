2589468ad9392694c7f6de734274e7e3898a7d23
r_kyo <hige3157@gmail.com>
2019-01-18 16:48:49 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   host = 'limitless-harbor-21156.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'ENV['GMAIL_USERNAME']',
